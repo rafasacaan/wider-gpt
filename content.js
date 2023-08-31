eff2a80677ffe30586538adf2cd624fb8782b50b
Rafael Sacaan A <rafasacaan@users.noreply.github.com>
2023-08-31 19:03:00 -0400

    Update content.js

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ var style = document.createElement('style');
 style.type = 'text/css';
 style.innerHTML = `
   .text-base {
-    max-width: 100% !important;
+    max-width: 80% !important;
     padding-left: 20px !important;
     padding-right: 20px !important;
   }
